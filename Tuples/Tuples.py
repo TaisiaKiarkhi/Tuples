4fe61de057858a59c5c07a7626e0b088a67e9b38
TaisaKiarkhi <tayisiyakiarkhi@gmail.com>
2023-10-02 16:14:53 +0200

    Add project files.

---
+++ Tuples/Tuples.py
@@ -0,0 +1,9 @@
+xy_pos = (2.3, 5.6) #tuples, immutable
+# xy_pos[1] = 3.3 wont work!
+# the common usage is for data which are never going to change
+list_of_xy = [(3.4, 2.1), (7.4, 8.9), (23.9, 78.1)]
+tuple_ = (3.5, 2.3, 9, 8)
+print(xy_pos)
+print(tuple_)
+print(list_of_xy[1][1])
+print(list_of_xy[2])
